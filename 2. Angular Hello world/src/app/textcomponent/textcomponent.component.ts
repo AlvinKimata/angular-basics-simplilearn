7ba93a59bd4cb7d7829954d84a67b162178763fd
Alvin Kimata <alvinkimata@gmail.com>
2023-01-19 13:47:30 +0300

    first commit

---
+++ 2. Angular Hello world/src/app/textcomponent/textcomponent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-textcomponent',
+  templateUrl: './textcomponent.component.html',
+  styleUrls: ['./textcomponent.component.css']
+})
+export class TextcomponentComponent {
+
+}
