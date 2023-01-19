a3060495e89fe0352a602bef6b4b91b0f0214a94
Alvin Kimata <alvinkimata@gmail.com>
2023-01-19 16:14:57 +0300

    update existing changes

--- 4. Angular Dependency Injection/dependency/src/app/emp-info/emp-info.component.ts
+++ 4. Angular Dependency Injection/dependency/src/app/emp-info/emp-info.component.ts
@@ -14,6 +14,18 @@ export class EmpInfoComponent {
   infoReceived2: string[] = [];
   infoReceived3: string[] = [];
 
+  getInfoFromServiceClass1() {
+    this.infoReceived1 = this.rservice.getinfo1();
+  }
+
+  getInfoFromServiceClass2() {
+    this.infoReceived2 = this.rservice.getinfo2();
+  }
+
+  getInfoFromServiceClass3() {
+    this.infoReceived3 = this.rservice.getinfo3();
+  }
+  
   constructor(private rservice: RecordsService) { }
 
   ngOnInit(): void {
