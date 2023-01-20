e79a1a5ccd1cd26310365c92fae4cb7ffd21b32c
Alvin Kimata <alvinkimata@gmail.com>
2023-01-20 15:52:20 +0300

    code demonstration for creating forms

--- 3. Angular components/democomponents/src/app/app.module.ts
+++ 3. Angular components/democomponents/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TextcomponentComponent } from './textcomponent/textcomponent.component';
 import { ImagecomponentsComponent } from './imagecomponents/imagecomponents.component';
+import { FormComponentComponent } from './form-component/form-component.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     TextcomponentComponent,
-    ImagecomponentsComponent
+    ImagecomponentsComponent,
+    FormComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
