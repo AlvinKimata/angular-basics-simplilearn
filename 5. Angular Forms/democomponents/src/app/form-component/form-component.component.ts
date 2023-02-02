aa859115ab1f10b0446a80ab1e01a3ca3a8a25a4
Alvin Kimata <alvinkimata@gmail.com>
2023-02-02 10:25:06 +0300

    add form components

---
+++ 5. Angular Forms/democomponents/src/app/form-component/form-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-component',
+  templateUrl: './form-component.component.html',
+  styleUrls: ['./form-component.component.css']
+})
+export class FormComponentComponent {
+
+}
