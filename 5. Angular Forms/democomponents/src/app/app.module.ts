4e2539ce883995bdefd073f802411aa4e9a65198
Alvin Kimata <alvinkimata@gmail.com>
2023-02-02 10:18:40 +0300

    creating forms in angular

--- 5. Angular Forms/democomponents/src/app/app.module.ts
+++ 5. Angular Forms/democomponents/src/app/app.module.ts
@@ -3,20 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TextcomponentComponent } from './textcomponent/textcomponent.component';
-import { ImagecomponentsComponent } from './imagecomponents/imagecomponents.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TextcomponentComponent,
-    ImagecomponentsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
