6b0ff02cacc0f3bbb4803632392253e3575a985c
Alvin Kimata <alvinkimata@gmail.com>
2023-02-02 12:23:13 +0300

    update existinf changes

--- 5. Angular Forms/democomponents/src/app/app.module.ts
+++ 5. Angular Forms/democomponents/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 import { FormComponentComponent } from './form-component/form-component.component';
 
 
--- 5. Angular Forms/democomponents/src/app/form-component/form-component.component.ts
+++ 5. Angular Forms/democomponents/src/app/form-component/form-component.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {enableProdMode} from '@angular/core';
 
 @Component({
   selector: 'app-form-component',
   templateUrl: './form-component.component.html',
   styleUrls: ['./form-component.component.css']
 })
-export class FormComponentComponent {
 
+export class FormComponentComponent {
+  ngOnInit(){
+  }
+  submit(){
+    debugger
+    console.log("Form submitted");
+  }
+  
 }
+
+
+
+
+enableProdMode();
