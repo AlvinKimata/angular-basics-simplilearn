94cf71347958ffb0a66c9982d378291c7d0be7c1
Alvin Kimata <alvinkimata@gmail.com>
2023-02-02 15:48:32 +0300

    add components to the main page

--- 7. Angular Bootstrap/democomponents/src/app/app.module.ts
+++ 7. Angular Bootstrap/democomponents/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { IntroComponent } from './Components/intro/intro.component';
+import { CoursesComponent } from './Components/courses/courses.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IntroComponent,
+    CoursesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
