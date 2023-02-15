65f9eb9d3510403fba0418a06dc47ce06fc28400
Alvin Kimata <alvinkimata@gmail.com>
2023-02-15 13:23:45 +0300

    property binding in angularjs

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'data-binding';
+	title = "Geeks";
+	classtype = "text-danger";
+	Geeks = "GeeksforGeeks";
+	image =
+"https://media.geeksforgeeks.org/wp-content/uploads/geeksforgeeks-6.png";
 }
